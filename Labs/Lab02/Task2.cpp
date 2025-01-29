f5c90cedde17094130de9f08a1daf252db255475
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2025-01-29 14:55:11 +0500

    Create Task2.cpp

---
+++ Labs/Lab02/Task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* arg[]) {
+        if (argc < 2) return 1;
+        int arr[atoi(arg[1])];
+        int i = 0;
+        for (i = 0; i < atoi(arg[1]); i++) {
+                arr[i] = atoi(arg[i+2]);
+        }
+        int missing_element = atoi(arg[i+2]);
+        int first_element = arr[0];
+        int difference = arr[1]-arr[0];
+        cout << missing_element  <<"th element  is " << first_element + (missin>
+        return 0;
+}
