81af130263f8f347d0d73c86ac42c832a238f214
Abser-Mansoor <fbsefigbhgnrihg@outlook.com>
2025-01-29 14:31:35 +0500

    Create Task1.cpp

---
+++ Labs/Lab02/Task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* arg[]) {
+        if (argc < 2) return 1;
+        int arr[atoi(arg[1])];
+        int sum = 0;
+        for (int i = 0; i < atoi(arg[1]); i++) {
+                arr[i] = atoi(arg[i+2]);
+                sum += atoi(arg[i+2]);
+        }
+        cout << "Sum is " <<  sum << endl;
+        cout << "Average is " << static_cast<float>(sum)/atoi(arg[1]);
+        return 0;
+}
+
+
+
